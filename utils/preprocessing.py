9e39bd9351812584959644bbbe53d7a5dc6f1bbf
kurapika12 <alam090206@gmail.com>
2026-06-11 12:02:52 +0800

    Add preprocessing functions for text cleaning, tokenizing, stopword removal, and stemming

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,5 +1,76 @@
+import re
+import string
+
+from nltk.corpus import stopwords
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+
+# ======================
+# STOPWORDS
+# ======================
+
+stop_words = set(stopwords.words("indonesian"))
+
+# ======================
+# STEMMER
+# ======================
+
+factory = StemmerFactory()
+stemmer = factory.create_stemmer()
+
+# ======================
+# PREPROCESSING
+# ======================
+
 def clean_text(text):
 
-    text = text.lower()
+    # Case Folding
+    text = str(text).lower()
 
-    return text
+    # Hapus URL
+    text = re.sub(r"http\S+|www\S+", "", text)
+
+    # Hapus angka
+    text = re.sub(r"\d+", "", text)
+
+    # Hapus tanda baca
+    text = text.translate(
+        str.maketrans(
+            "",
+            "",
+            string.punctuation
+        )
+    )
+
+    # Hapus spasi berlebih
+    text = re.sub(
+        r"\s+",
+        " ",
+        text
+    ).strip()
+
+    # ======================
+    # TOKENIZING
+    # ======================
+
+    tokens = text.split()
+
+    # ======================
+    # STOPWORD REMOVAL
+    # ======================
+
+    tokens = [
+        word
+        for word in tokens
+        if word not in stop_words
+    ]
+
+    # ======================
+    # STEMMING
+    # ======================
+
+    stemmed_tokens = [
+        stemmer.stem(word)
+        for word in tokens
+    ]
+
+    return " ".join(stemmed_tokens)
